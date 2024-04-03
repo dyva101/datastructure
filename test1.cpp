a96163d3eade75757fc6a682604b4d212e0ef90e
dyva101 <davyuser456@gmail.com>
2024-04-03 00:21:47 -0300

    problema 3 - test done

--- test1.cpp
+++ test1.cpp
@@ -47,3 +47,50 @@ int main() {
 
     return 0;
 }   
+
+//Problema 3
+
+void operarArrays(int iArray_1[5][5], int iArray_2[5][5], char cOperacao) {
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            switch (cOperacao) {
+                case '+':
+                    iArray_1[i][j] += iArray_2[i][j];
+                    break;
+                case '-':
+                    iArray_1[i][j] -= iArray_2[i][j];
+                    break;
+            }
+        }
+    }
+}
+
+int main() {
+    int iArray_1[5][5], iArray_2[5][5];
+    char cOperacao;
+    int b[5][5] = {
+        {1, 2, 3, 4, 5},
+        {6, 7, 8, 9, 10},
+        {11, 12, 13, 14, 15},
+        {16, 17, 18, 19, 20},
+        {21, 22, 23, 24, 25}
+    };
+
+    int c[5][5] = {
+        {1, 2, 3, 4, 5},
+        {6, 1, 8, 9, 10},
+        {11, 12, 1, 14, 15},
+        {16, 17, 18, 1, 20},
+        {21, 22, 23, 24, 1}
+    };
+
+    operarArrays(b, c, '+');
+
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            cout << b[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+}
